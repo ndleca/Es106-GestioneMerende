38a1ded2265f296febf469cecf841c91cf602b57
ndleca <andreileca54@yahoo.it>
2015-05-07 11:01:58 +0200

    Update Schifezze.java

--- src/gestionemerende/Schifezze.java
+++ src/gestionemerende/Schifezze.java
@@ -2,5 +2,9 @@ package gestionemerende;
 
 public class Schifezze extends Articolo
 {
-	
+
+	public Schifezze(double apportoCalorico, String codice,	double costoUnitario, String descrizione, int a) {
+		super(apportoCalorico, codice, costoUnitario, descrizione);
+	}
 }
+
